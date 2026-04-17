b20c6c6a9c56d897f85675fc0ac680fdbb499eb0
Yuri Sklyarov <yuri.mcalex@gmail.com>
2026-04-17 15:49:52 +0300

    Create an audio element to play audio

---
+++ src/audio.js
@@ -0,0 +1,4 @@
+import track from './assets/track.mp3'
+const player = document.getElementById('audio');
+
+player.src = track;
--- src/main.js
+++ src/main.js
@@ -1,2 +1,3 @@
 import './style.css'
 import { getCanvasCtx } from './canvas.js';
+import { } from './audio.js';
