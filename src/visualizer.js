05c45902437b193533739da4d14dd3319822a383
Yuri Sklyarov <yuri.mcalex@gmail.com>
2026-04-18 17:30:16 +0300

    Create a vertical bar audio frequency visualizer

---
+++ src/visualizer.js
@@ -0,0 +1,31 @@
+export function visualizeSpectrum(freq, ctx, canvasDimensions) {
+  let x = 0;
+
+  for (let i = 0; i < freq.length; i++) {
+    const options = {
+      ind: i,
+      dataLen: freq.length,
+      value: freq[i],
+      ctx: ctx,
+      canvasW: canvasDimensions.w,
+      canvasH: canvasDimensions.h,
+      shiftX: x,
+      barWidth: (canvasDimensions.w / freq.length) * 1.19 // * 1.19 to hide zero freq
+    };
+
+    ctx.fillStyle = calcColor(options);
+    drawBar(options);
+
+    x += options.barWidth;
+  }
+}
+
+
+function drawBar({ ctx, value, canvasH, shiftX, barWidth }) {
+  ctx.fillRect(shiftX, canvasH - value, barWidth - 1, value);
+}
+
+
+function calcColor({ ind, dataLen, value }) {
+  return `rgb(${value + 25}, ${250 * (ind / dataLen)}, ${50})`;
+}
