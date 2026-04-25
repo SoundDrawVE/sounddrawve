ab93648fe50233c3eeec224428b7f432c6302034
Yuri Sklyarov <yuri.mcalex@gmail.com>
2026-04-25 12:17:52 +0300

    Handle the aspect ratio selection event

--- src/settings.js
+++ src/settings.js
@@ -10,6 +10,24 @@ export const settings = {
   },
 
   setProp(name, value) {
+    console.log(name, value);
     this[name] = value;
   }
 };
+
+const form = document.getElementById('settings-form');
+form.addEventListener('input', (e) => {
+  const field = e.target;
+  const fieldName = toCamelCase(field.name);
+  
+  settings.setProp(fieldName, field.value);
+
+  if (fieldName === 'aspectRatio') {
+    window.dispatchEvent(new Event('resize'));
+  }
+});
+
+
+function toCamelCase(str) {
+  return str.replace(/[-_](.)/g, (_, char) => char.toUpperCase());
+}
