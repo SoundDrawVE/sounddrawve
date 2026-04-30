599f85ee53704bbb9f797ec317673e7205ea6589
Yuri Sklyarov <yuri.mcalex@gmail.com>
2026-04-30 14:48:02 +0300

    Create a function to visualize the falling of a droplet or snowflake

--- src/visualizer.js
+++ src/visualizer.js
@@ -30,12 +30,71 @@ export function visualizeSpectrum(freq, ctx) {
     }
 
     if (settings.visualizationType === 'bars') {
-      drawBar(options);
+      drawDrop(options);
     } else if (settings.visualizationType === 'stripes') {
       drawStripe(options);
     }
 
     x += options.barWidth;
+  }
+}
+
+const tmpData = {
+  data: [],
+
+  reset() { 
+    this.data = [];
+  },
+
+  getValue(ind) {
+    return this.data[ind];
+  },
+
+  setValue(ind, value) {
+    this.data[ind] = value;
+  }
+};
+
+settings.onChange(() => tmpData.reset());
+
+
+function drawDrop({ ind, ctx, value, canvasW, canvasH, areaW, areaH, areaX, areaY, shiftX }) {
+  const k = (areaW * areaH) /(canvasW * canvasH);
+  const maxSpeed = 5;
+  let flakeCoords = tmpData.getValue(ind);
+
+  if (!flakeCoords) {
+    flakeCoords = calcInitialCoords();
+  }
+
+  updateCoords();
+  ctx.beginPath();
+  ctx.arc(flakeCoords.x, flakeCoords.y, flakeCoords.r, 0, 2 * Math.PI);
+  ctx.fill();
+  tmpData.setValue(ind, flakeCoords);
+
+  function updateCoords() {
+    let speedY = value * maxSpeed / 255;
+    let speedX = speedY / 3;
+    if (speedY < 1) {
+      speedY = 1;
+      speedX = 1 / 3;
+    }
+
+    flakeCoords.x -= speedX;
+    flakeCoords.y += speedY;
+
+    if (flakeCoords.x < areaX || flakeCoords.y > areaY + areaH) {
+      flakeCoords = calcInitialCoords();
+    }
+  }
+
+  function calcInitialCoords() {
+    return {
+      x: randInt(areaX, areaX + areaW),
+      y: randInt(areaY, areaY + areaH),
+      r: value * 0.015 * Math.exp(k)
+    };
   }
 }
 
@@ -57,4 +116,10 @@ function drawBar({ ctx, value, canvasH, areaH, areaY, shiftX, barWidth }) {
 
 function calcColor({ ind, dataLen, value }) {
   return `rgb(${value + 25}, ${250 * (ind / dataLen)}, ${50})`;
+}
+
+
+function randInt(min, max) {
+  let rand = min - 0.5 + Math.random() * (max - min + 1);
+  return Math.round(rand);
 }
