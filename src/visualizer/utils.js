e9de395b581fa57b3a5bbacda536565e8b3385a0
Yuri Sklyarov <yuri.mcalex@gmail.com>
2026-05-27 12:06:38 +0300

    Create avg and getBass common fns

--- src/visualizer/utils.js
+++ src/visualizer/utils.js
@@ -2,3 +2,13 @@ export function randInt(min, max) {
   let rand = min - 0.5 + Math.random() * (max - min + 1);
   return Math.round(rand);
 }
+
+export function avg(arr, start = 0, end = arr.length) {
+  let s = 0;
+  for (let i = start; i < end; i++) s += arr[i];
+  return s / (end - start);
+}
+
+export function getBass(freq) {
+  return avg(freq, 0, freq.length * 0.08) / 255;
+}
