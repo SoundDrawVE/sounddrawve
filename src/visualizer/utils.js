2c13775dc9e87047c63d11ab5c5ef3eac2e1be4c
Yuri Sklyarov <yuri.mcalex@gmail.com>
2026-05-28 14:36:11 +0300

    Create rgba to shl converter fn

--- src/visualizer/utils.js
+++ src/visualizer/utils.js
@@ -16,3 +16,23 @@ export function getBass(freq) {
 export function getMids(freq) {
   return avg(freq, freq.length * 0.08, freq.length * 0.35) / 255;
 }
+
+export function rgbaToHsl(r, g, b) {
+    r /= 255; g /= 255; b /= 255;
+    const max = Math.max(r, g, b), min = Math.min(r, g, b);
+    let h, s, l = (max + min) / 2;
+
+    if (max === min) {
+        h = s = 0; // ахроматический
+    } else {
+        const d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch (max) {
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
+    return { h: h * 360, s: s * 100, l: l * 100 };
+}
